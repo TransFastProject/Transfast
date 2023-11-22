739c4bdbdfdf15435cc48988c60e038c470cdb9e
“Winnie <winniestefany303@gmail.com>
2023-11-22 00:30:49 -0300

    update seu_transporte_com

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const a = document.getElementById("card1")
 const buttonClose = document.getElementById("fechar-modal")
-const modal = document.querySelector("dialog")
+const modal = document.getElementById("home-dialog")
 
 a.onclick = function () {
     modal.showModal()
@@ -10,3 +10,16 @@ buttonClose.onclick = function () {
     modal.close();
 }
 
+const avaliar = document.getElementById("avaliar")
+const avaliarClose = document.getElementById("avaliar-close")
+const modalAvaliar = document.querySelector("modal-avaliacao")
+const bodyDialog = document.getElementById("fundoDialog")
+
+avaliar.onclick = function () {
+    modalAvaliar.showModal();
+    bodyDialog.style.display = "block";
+};
+
+avaliarClose.onclick = function () {
+    modalAvaliar.close();
+}
